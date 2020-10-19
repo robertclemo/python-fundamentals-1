d42bab19a84c2d6f3db81ef8d29bbe4dfd04a4dd
Dunieski Otano <dunieskior@gmail.com>
2020-10-19 13:29:15 -0400

    Strings

---
+++ Strings/strings.py
@@ -0,0 +1,6 @@
+
+print('This is simple quotes - "Really?"') # => prints This is simple quotes - "Really?"
+print("Double quotes inside \"double\" quotes") # => prints Double quotes inside "double" quotes
+print('Single quotes inside \'single\' quotes') # => prints Single quotes inside 'single' quotes
+print("It's is double quotes") # => prints It's is double quotes
+print('''This is triple quotes''') # => prints This is triple quotes
