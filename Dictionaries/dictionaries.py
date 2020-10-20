0b0f1e0652228994ea4b6b8cbb54a68c6e92c216
Dunieski Otano <dunieskior@gmail.com>
2020-10-20 16:37:07 -0400

    Dictionaries

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,36 @@
+import os
+os.system('clear')
+
+myNewDisctionary = {
+    "Integer": 4,
+    "Float": 3.6,
+    "String": "I am here",
+    "Boolean": True,
+    "FirstName": "Jorge",
+    "LastName": 'Martinez', 
+    "Age": 79
+}
+
+print(myNewDisctionary)
+# Print first element:
+
+print(f'First Element: {myNewDisctionary["Integer"]}')
+
+# Print every element of the dictionary
+
+keys = myNewDisctionary.keys() # this will get the keys
+values = myNewDisctionary.values() # this will get the values
+print(f'{keys} - {values}')
+
+
+print(keys)
+for element in myNewDisctionary:
+    print(f' - {myNewDisctionary[element]}')
+
+for element in myNewDisctionary:
+    print(myNewDisctionary.get(element))
+
+
+# Prints key and value from Dictionary
+for i in myNewDisctionary:
+    print(i, myNewDisctionary[i])
