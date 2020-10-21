e1aa92608510e6e3b273df07124acbdd3d53d399
Dunieski Otano <dunieskior@gmail.com>
2020-10-21 12:53:39 -0400

    Functions

--- Funtions/functions.py
+++ Funtions/functions.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 os.system('clear')
 
 # A CUSTOM function can take as many arguments as you want 
@@ -45,6 +45,7 @@ print(text)
 print('This is the text that I got:', text)
 
 
+
 # calling a function without any arguments => function returns something
 def add():
     addition = 4 + 6 # addition is a local variable
@@ -82,3 +83,35 @@ print('''
 '''.format(add, sub, mul, div, mod))
 
 print(returnMultipleValue(3, 5))
+
+# Working with the scope of variables
+x = 'fantastic' # outer world
+
+def printLocal():
+    x = 'awesome' # inner world
+    print('Local x =', x) # inner world
+
+printLocal()# outer world
+print('Global x =', x) # outer world
+
+def printOptionalArguments(num1 = None, num2 = None):
+    if (num1 == None):
+        num1 = int(input('I see you are not passing any number for num1. Enter a number >> '))
+    if (num2 == None):
+        num2 = int(input('I see you are not passing any number for num2. Enter another number >> '))
+    addition = num1 + num2
+    print('The sum of', num1, 'and', num2, 'is', addition)
+
+os.system('clear')
+print('Calling printOptionalArguments() for the first time')
+time.sleep(5)
+printOptionalArguments()
+print(' ')
+print('Calling printOptionalArguments(5, 7) for the second time, passing 5 and 7')
+time.sleep(5)
+printOptionalArguments(5, 7)
+print(' ')
+print('Calling printOptionalArguments(7) for the third time, passing 7 alone => it matches num1 in the function')
+time.sleep(5)
+printOptionalArguments(7)
+
